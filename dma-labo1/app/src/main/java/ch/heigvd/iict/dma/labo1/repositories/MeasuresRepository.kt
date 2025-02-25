d7c7fcc1c553a25822be5a38fc681f7679411def
neroil <edwin.haffner@heig-vd.ch>
2025-02-25 11:49:00 +0100

    XML now works

--- dma-labo1/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
+++ dma-labo1/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
@@ -6,12 +6,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
 import ch.heigvd.iict.dma.labo1.models.*
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.net.URL
+import org.jdom2.DocType
+import org.jdom2.Document
+import org.jdom2.Element
+import org.jdom2.input.SAXBuilder
+import org.jdom2.output.XMLOutputter
+import java.io.StringReader
 import java.net.HttpURLConnection
+import java.net.URL
 import kotlin.system.measureTimeMillis
 
 class MeasuresRepository(private val scope : CoroutineScope,
@@ -73,7 +78,7 @@ class MeasuresRepository(private val scope : CoroutineScope,
 
                 val body = when (serialisation) {
                     Serialisation.JSON -> gson.toJson(measures.value)
-                    Serialisation.XML -> TODO()
+                    Serialisation.XML -> toXML()
                     Serialisation.PROTOBUF -> TODO()
                 }
 
@@ -96,13 +101,82 @@ class MeasuresRepository(private val scope : CoroutineScope,
                 os.close()
 
                 // Récupère la réponse
-                val json = con.inputStream.bufferedReader().use { it.readText() }
-                Log.d("Req", json)
+                val response = con.inputStream.bufferedReader().use { it.readText() }
+
+                when (serialisation){
+                    Serialisation.JSON -> {
+                        Log.d("Req", "response: $response")
+                        val statusList = gson.fromJson(response, Array<Response>::class.java)
+                        for (status in statusList){
+                            updateMeasureStatus(Measure.Status.valueOf(status.status!!), status.id!!.toInt())
+                        }
+                    }
+                    Serialisation.XML -> {
+
+                        try {
+                            val builder = SAXBuilder()
+                            // Crashes if not set to false
+                            builder.setExpandEntities(false)
+
+                            val responseDoc = builder.build(StringReader(response))
+                            val measureElements = responseDoc.rootElement.getChildren("measure")
+
+                            for (measureElement in measureElements) {
+                                val id = measureElement.getAttributeValue("id").toInt()
+                                val status = Measure.Status.valueOf(measureElement.getAttributeValue("status"))
+                                updateMeasureStatus(status, id)
+                            }
+                        } catch (e: Exception) {
+                            Log.e("XML", "Error parsing XML: ${e.message}", e)
+                        }
+                    }
+                    Serialisation.PROTOBUF -> TODO()
+                }
+
                 //val type = object : TypeToken<Response>() {}.type
                 //Gson().fromJson<Response>(json, type)
             }
             _requestDuration.postValue(elapsed)
         }
     }
+
+    fun toXML(): String{
+        try{
+            val doc = Document()
+            doc.setRootElement(Element("measures"))
+            doc.setDocType(DocType("measures",dtd))
+            measures.value?.forEach {
+                //Measure
+                val measure = Element("measure")
+                measure.setAttribute("id", it.id.toString())
+                measure.setAttribute("status", it.status.toString())
+                //Type
+                measure.addContent(Element("type").addContent(it.type.toString()))
+                //Value
+                measure.addContent(Element("value").addContent(it.value.toString()))
+                //Date
+                measure.addContent(Element("date").addContent(it.date.toString()))
+                doc.rootElement.addContent(measure)
+            }
+
+            return XMLOutputter().outputString(doc)
+
+        } catch (e: Exception){
+            Log.e("XML", e.toString())
+            return ""
+        }
+    }
+
+    private fun updateMeasureStatus(status: Measure.Status, id: Int){
+        val l = _measures.value!!
+        for (measure in l) {
+            if (measure.id == id) {
+                measure.status = status
+                _measures.postValue(l)
+                break
+            }
+        }
+    }
+
 
 }
